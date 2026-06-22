b82575faa6fbb4d6df0f3dbd83147513ed5a8793
InnaMoroshkinaI42 <innamoroshkina@yandex.ru>
2026-06-22 18:16:55 +0700

    готовая версия 01112025

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './index.css';
+
+const menu = document.querySelector('.mobile-menu__inner');
+const menuLinks = document.querySelectorAll('.mobile-menu a');
+
+document.querySelector('.js-menu-active').addEventListener('click', () => {
+  const isActive = menu.classList.toggle('mobile-menu__inner--active');
+  document.body.style.overflow = isActive ? 'hidden' : '';
+});
+
+// Закрытие меню при клике на ссылку
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('mobile-menu__inner--active');
+    document.body.style.overflow = '';
+  });
+});
